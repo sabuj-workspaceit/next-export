b98b4722c09819d49f8501268a27d51e9fac827c
Sabuj Islam <sabuj@workspaceit.com>
2025-12-05 17:22:38 +0600

    config reset

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: "/next-export/out",
-  assetPrefix: "/next-export/out/",
+  // basePath: "/next-export/out",
+  // assetPrefix: "/next-export/out/",
 };
 
 export default nextConfig;
