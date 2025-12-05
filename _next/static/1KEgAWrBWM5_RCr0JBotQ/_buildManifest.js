fc6a845212c93acaaeba2cbe6acdcc2a57fc380d
Sabuj Islam <sabuj@workspaceit.com>
2025-12-05 16:56:31 +0600

    new build

---
+++ _next/static/1KEgAWrBWM5_RCr0JBotQ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/next-export//_next/:path+",
+        "destination": "/next-export/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: "/next-export/out",
-  assetPrefix: "/next-export/out/",
-};
-
-export default nextConfig;
